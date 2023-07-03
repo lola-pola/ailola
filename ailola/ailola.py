b52f8434ea87e3ef25aadfed40ea8421dbaf89eb
mamoyal <97595015+MatanAmoyal1@users.noreply.github.com>
2023-07-03 16:36:25 +0300

    Update ailola.py ad jenkins console debug

--- ailola/ailola.py
+++ ailola/ailola.py
@@ -4,7 +4,9 @@ import os
 import openai
 import click
 import colorama
+import requests
 from colorama import Fore, Style
+
 
 
 def create_terraform(query,provider,temperature):
@@ -73,18 +75,52 @@ def validate_terraform(query,provider,temperature):
 
 
 
+def debug_url(query,provider,temperature):
+    if provider == "azure":
+        openai.api_type = "azure"
+        openai.api_base = os.getenv("OPENAI_API_BASE")
+        openai.api_version = "2023-03-15-preview"
+        openai.api_key = os.getenv("OPENAI_API_KEY")
+        engine =  os.getenv("OPENAI_ENGINE")
+    else:
+        openai.api_key = os.getenv("OPENAI_API_KEY")
+        engine =  os.getenv("OPENAI_ENGINE")
+    if provider == "azure":   
+        response = openai.ChatCompletion.create(
+        engine=engine,
+        messages = [{"role":"system","content":"You are a validator for jenkins, only validate jenkins console log, \
+                    You are a Jenkins validator and debug current console \
+                    "},{"role":"user","content":query}],
+        temperature=temperature,
+        stop=None)
+    else:
+        response = openai.ChatCompletion.create(
+        model=engine,
+        messages = [{"role":"system","content":"You are a validator for jenkins, only validate jenkins console log, \
+                    You are a Jenkins validator and debug current console \
+                    "},{"role":"user","content":query}],
+        temperature=temperature,
+        stop=None)
+    
+    return str(response['choices'][0]['message']['content'])
+
+
 
 @click.command()
 @click.option('--data', type=(str))
 @click.option('--validate',type=(str))
+@click.option('--debug',type=(str))
 @click.option('--provider', required=True, type=(str),default="azure")
 @click.option('--temperature', type=(float),default=1.0)
-def cli(data,provider,validate,temperature):
+def cli(data,provider,validate,temperature, debug):
     if validate:
         with open(validate, 'r') as f:
             res = validate_terraform(query=f.read(),provider=provider,temperature=temperature)
             click.echo(Fore.YELLOW +str(res))
 
+    elif debug:
+        res = requests.get(debug)
+        debug_url(query=res.text(),provider=provider,temperature=temperature)
     else:
         res = create_terraform(query=data,provider=provider,temperature=temperature)
         click.echo(Fore.BLUE +str(res))
