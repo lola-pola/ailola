86b1762a92b5d9c848e9323dad6d28cc96383ebc
shdowofdeath <elhayefrat@gmail.com>
2023-07-04 11:02:57 +0300

    asdd¸:wq
    Signed-off-by: shdowofdeath <elhayefrat@gmail.com>

--- ailola/ailola.py
+++ ailola/ailola.py
@@ -9,7 +9,10 @@ from colorama import Fore, Style
 
 
 
-def create_terraform(query,provider,temperature):
+
+
+
+def general_prompt(general_query,provider,temperature):
     if provider == "azure":
         openai.api_type = "azure"
         openai.api_base = os.getenv("OPENAI_API_BASE")
@@ -22,87 +25,18 @@ def create_terraform(query,provider,temperature):
     if provider == "azure":   
         response = openai.ChatCompletion.create(
         engine=engine,
-        messages = [{"role":"system","content":"You are a file name generator, only generate valid names for Terraform templates, \
-                    You are a Terraform HCL generator, only generate valid Terraform HCL without provider \
-                        templates."},{"role":"user","content":query}],
+        messages = general_query,
         temperature=temperature,
         stop=None)
     else:
         response = openai.ChatCompletion.create(
         model=engine,
-        messages = [{"role":"system","content":"You are a file name generator, only generate valid names for Terraform templates, \
-                    You are a Terraform HCL generator, only generate valid Terraform HCL \
-                        templates."},{"role":"user","content":query}],
+        messages = general_query,
         temperature=temperature,
         stop=None)
     
     return str(response['choices'][0]['message']['content'])
 
-
-
-
-
-def validate_terraform(query,provider,temperature):
-    if provider == "azure":
-        openai.api_type = "azure"
-        openai.api_base = os.getenv("OPENAI_API_BASE")
-        openai.api_version = "2023-03-15-preview"
-        openai.api_key = os.getenv("OPENAI_API_KEY")
-        engine =  os.getenv("OPENAI_ENGINE")
-    else:
-        openai.api_key = os.getenv("OPENAI_API_KEY")
-        engine =  os.getenv("OPENAI_ENGINE")
-    if provider == "azure":   
-        response = openai.ChatCompletion.create(
-        engine=engine,
-        messages = [{"role":"system","content":"You are a file name validator, only validate Terraform templates, \
-                    You are a Terraform HCL validator and suggest security changes and terraform changes \
-                        templates."},{"role":"user","content":query}],
-        temperature=temperature,
-        stop=None)
-    else:
-        response = openai.ChatCompletion.create(
-        model=engine,
-        messages = [{"role":"system","content":"You are a file name validator, only validate Terraform templates, \
-                    You are a Terraform HCL validator \
-                        templates."},{"role":"user","content":query}],
-        temperature=temperature,
-        stop=None)
-    
-    return str(response['choices'][0]['message']['content'])
-
-
-
-
-
-def debug_url(query,provider,temperature):
-    if provider == "azure":
-        openai.api_type = "azure"
-        openai.api_base = os.getenv("OPENAI_API_BASE")
-        openai.api_version = "2023-03-15-preview"
-        openai.api_key = os.getenv("OPENAI_API_KEY")
-        engine =  os.getenv("OPENAI_ENGINE")
-    else:
-        openai.api_key = os.getenv("OPENAI_API_KEY")
-        engine =  os.getenv("OPENAI_ENGINE")
-    if provider == "azure":   
-        response = openai.ChatCompletion.create(
-        engine=engine,
-        messages = [{"role":"system","content":"You are a validator for jenkins, only validate jenkins console log, \
-                    You are a Jenkins validator and debug current console \
-                    "},{"role":"user","content":query}],
-        temperature=temperature,
-        stop=None)
-    else:
-        response = openai.ChatCompletion.create(
-        model=engine,
-        messages = [{"role":"system","content":"You are a validator for jenkins, only validate jenkins console log, \
-                    You are a Jenkins validator and debug current console \
-                    "},{"role":"user","content":query}],
-        temperature=temperature,
-        stop=None)
-    
-    return str(response['choices'][0]['message']['content'])
 
 
 
@@ -110,22 +44,38 @@ def debug_url(query,provider,temperature):
 @click.option('--data', type=(str))
 @click.option('--validate',type=(str))
 @click.option('--debug',type=(str))
+@click.option('--kuba',type=(str))
 @click.option('--provider', required=True, type=(str),default="azure")
 @click.option('--temperature', type=(float),default=1.0)
-def cli(data,provider,validate,temperature, debug):
+def cli(data,provider,validate,temperature, debug,kuba):
+    
+    def get_relevant(key,query):
+        prompt = {'create_terraform':[{"role":"system","content":"You are a file name generator, only generate valid names for Terraform templates,You are a Terraform HCL generator, only generate valid Terraform HCL templates."},{"role":"user","content":query}],
+                  'validate_terraform':[{"role":"system","content":"You are a Terraform HCL validator, only validate valid Terraform HCL templates."},{"role":"user","content":query}],
+                  'create_k8s':[{"role":"system","content":"You are a k8s template generator, only generate valid k8s templates."},{"role":"user","content":query}],
+                  'debug' : [{"role":"system","content":"You are a validator for jenkins, only validate jenkins console log, \You are a Jenkins validator and debug current console "},{"role":"user","content":query}]
+
+              }
+        return prompt[key]
+    
+    
     if validate:
         with open(validate, 'r') as f:
-            res = validate_terraform(query=f.read(),provider=provider,temperature=temperature)
+            res = general_prompt(general_query=get_relevant(key="validate_terraform",query=f.read()),provider=provider,temperature=temperature)
             click.echo(Fore.YELLOW +str(res))
 
     elif debug:
         res = requests.get(debug)
-        res = debug_url(query=res.text,provider=provider,temperature=temperature)
-        click.echo(Fore.YELLOW +str(res))
+        res = general_prompt(general_query=get_relevant(key="debug",query=res.text),provider=provider,temperature=temperature)
+        click.echo(Fore.GREEN +str(res))
+    elif kuba:
+        res = general_prompt(general_query=get_relevant(key="create_k8s",query=kuba),provider=provider,temperature=temperature)
+        click.echo(Fore.BLUE +str(res))
     else:
-        res = create_terraform(query=data,provider=provider,temperature=temperature)
+        res = general_prompt(general_query=get_relevant(key="create_terraform",query=data),provider=provider,temperature=temperature)
         click.echo(Fore.BLUE +str(res))
     
     
 if __name__ == '__main__':
     cli()
+
